b0053f1a8d139f5c4f199c914b2d3caac152a2dd
Bob Tanner <tanner@real-time.com>
2018-05-22 23:47:48 -0500

    Extracted the .msi from the .exe and use that for the installation.

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,25 +1,27 @@
 ﻿$ErrorActionPreference = 'Stop'; 
 
+# Extract the msi from the exe with SYSPROReportingComponentsCR2013.exe /a /b "."
+
 $toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileLocation   = Join-Path $toolsDir 'SYSPROReportingComponentsCR2013.exe'
+$fileLocation   = Join-Path $toolsDir 'SYSPROReportingComponentsCR2013.msi'
+
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
   unzipLocation = $toolsDir
-  fileType      = 'EXE' 
+  fileType      = 'MSI' 
   file          = $fileLocation
   softwareName  = 'syspro-reporting-components*'
 
   # Checksums are now required as of 0.10.0.
   #
-  checksum      = 'caf4facb77d66496f44ef99563cc43da6777dc9fc2e7f0b1778667293b858b6d'
+  checksum      = '23983d11ba2503779d39f86af0b736747af7b9e86d56de966dd2e622c1078d92'
   checksumType  = 'sha256'
 
   # Cygwin $env:TEMP is C:\tools\cygwin\tmp\chocolatey
   # Powershell $env:TEMP is C:\Users\tanner\AppData\Local\Temp
   #
-  #silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).log`""
-  silentArgs     = "/s /v`"/qb/norestart`""
+  silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).log`""
   validExitCodes = @(0, 3010, 1641)
 }
 
