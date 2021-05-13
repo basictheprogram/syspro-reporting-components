93acb7aaf74ba6207690c8053cad48efba01a477
Bob Tanner <tanner@real-time.com>
2021-05-13 14:14:24 -0500

    Update chocolateyinstall.ps1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -7,7 +7,7 @@ $packageArgs = @{
     packageName    = $env:ChocolateyPackageName
     fileType       = 'exe'
     silentArgs     = "/S /v/qn"
-    url            = 'http://gold-images.int.celadonsystems.com/SYSPRO/CrystalRuntimes/SYSPROReportingComponentsCR2013.exe'
+    url            = ''
     checksum       = 'caf4facb77d66496f44ef99563cc43da6777dc9fc2e7f0b1778667293b858b6d'
     checksumType   = 'sha256'
 }
