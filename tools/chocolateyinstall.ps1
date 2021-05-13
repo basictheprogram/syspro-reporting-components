4b22e511c6cb070953c9d4023b404169bd5a8d1b
Bob Tanner <tanner@real-time.com>
2021-05-13 13:56:07 -0500

    * Changed dependencies to Crystal Reports 2013 Runtime   * Changed to url for package   * Trivial formatting changes

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,30 +1,17 @@
-﻿$ErrorActionPreference = 'Stop'; 
+﻿$ErrorActionPreference = 'Stop';
 
-# Extract the msi from the exe with SYSPROReportingComponentsCR2013.exe /a /b "."
-
-$toolsDir       = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileLocation   = Join-Path $toolsDir 'SYSPROReportingComponentsCR2013.msi'
-
+# Extract the msi from the exe with SYSPROReportingComponentsCR2013.exe /s /x /b"." /v"/qn"
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  unzipLocation = $toolsDir
-  fileType      = 'MSI' 
-  file          = $fileLocation
-  softwareName  = 'syspro-reporting-components*'
-
-  # Checksums are now required as of 0.10.0.
-  #
-  checksum      = '23983d11ba2503779d39f86af0b736747af7b9e86d56de966dd2e622c1078d92'
-  checksumType  = 'sha256'
-
-  # Cygwin $env:TEMP is C:\tools\cygwin\tmp\chocolatey
-  # Powershell $env:TEMP is C:\Users\tanner\AppData\Local\Temp
-  #
-  silentArgs     = "/qn /norestart /l*v `"$($env:TEMP)\$($packageName).log`""
-  validExitCodes = @(0, 3010, 1641)
+    packageName    = $env:ChocolateyPackageName
+    fileType       = 'exe'
+    silentArgs     = "/S /v/qn"
+    url            = ''
+    checksum       = 'caf4facb77d66496f44ef99563cc43da6777dc9fc2e7f0b1778667293b858b6d'
+    checksumType   = 'sha256'
 }
 
 # https://chocolatey.org/docs/helpers-install-chocolatey-install-package
 #
-Install-ChocolateyInstallPackage @packageArgs 
+Install-ChocolateyPackage @packageArgs
