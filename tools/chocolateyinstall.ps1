787937d62ecbaabb8f17193a21b9d04c572439bd
Bob Tanner <tanner@real-time.com>
2021-06-10 15:35:16 -0500

    WIP: #8
      * Remove private information to the data.ps1
      * Ignore the data.ps1 so it's not commited to git
      * Updated the sha256

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,15 +1,16 @@
 ﻿$ErrorActionPreference = 'Stop';
 
 # Extract the msi from the exe with SYSPROReportingComponentsCR2013.exe /s /x /b"." /v"/qn"
+$data = & (Join-Path -Path (Split-Path -Path $MyInvocation.MyCommand.Path) -ChildPath data.ps1)
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
-    packageName    = $env:ChocolateyPackageName
-    fileType       = 'exe'
-    silentArgs     = "/S /v/qn"
-    url            = 'http://gold-images.int.celadonsystems.com/SYSPRO/CrystalRuntimes/SYSPROReportingComponentsCR2013.exe'
-    checksum       = 'caf4facb77d66496f44ef99563cc43da6777dc9fc2e7f0b1778667293b858b6d'
-    checksumType   = 'sha256'
+    packageName  = $env:ChocolateyPackageName
+    fileType     = 'exe'
+    silentArgs   = "/S /v/qn"
+    url          = $data.url
+    checksum     = $data.checksum
+    checksumType = $data.checksumType
 }
 
 # https://chocolatey.org/docs/helpers-install-chocolatey-install-package
